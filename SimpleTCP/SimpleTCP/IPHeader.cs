f2ca78db9829d7581f183a5120bf3ff58a558a18
rcisneros138@gmail.com <rcisneros138@gmail.com>
2015-12-09 16:57:41 -0600

    Working on TCP header

---
+++ SimpleTCP/SimpleTCP/IPHeader.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTCP
+{
+    class IPHeader
+    {
+        ushort version;
+        ushort IHL;
+        ushort typeOfService;
+        ushort totalLength;
+        ushort identification;
+        ushort flags;
+        ushort fragmentOffset;
+        ushort timeToLive;
+        ushort protocol; // =6 ??
+        ushort Checksum;
+        ushort sourceIP;
+        ushort destinationIP;
+        ushort options;
+        ushort padding;
+    }
+}
---
+++ SimpleTCP/SimpleTCP/TcpHeader.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace SimpleTCP
+{
+   public class TcpHeader
+    {
+        public TcpHeader()
+        {
+            BitArray sourcePort = new BitArray(16);
+            BitArray destinationPort = new BitArray(16);
+            BitArray sequencePort = new BitArray(32);
+            BitArray acknowlegementNumber = new BitArray(32);
+            BitArray dataOffset = new BitArray(4);  //needs to be 4 bits
+            BitArray reversed = new BitArray(3); //needs to be 6 bits
+            BitArray controlBits = new BitArray(6); //needs to be 6 bits
+            BitArray CWR = new BitArray(1)
+            ushort window;
+            ushort checkSum;
+            ushort urgentPointer;
+            ushort tcpOptions;
+            ushort padding;
+            ushort tcpData;
+
+        }
+
+
+
+
+
+        public static void ConstructPacket()
+        {
+            
+        }
+
+    }
+}
