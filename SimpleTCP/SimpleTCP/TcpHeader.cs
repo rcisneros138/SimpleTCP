cab89193da2b533a057f702ffce38a403deb8039
rcisneros138@gmail.com <rcisneros138@gmail.com>
2015-12-09 18:02:26 -0600

    added on to TCP constructor

--- SimpleTCP/SimpleTCP/TcpHeader.cs
+++ SimpleTCP/SimpleTCP/TcpHeader.cs
@@ -9,22 +9,21 @@ namespace SimpleTCP
 {
    public class TcpHeader
     {
-        public TcpHeader()
+        public TcpHeader(int sourcePort, int destinationPort, int sequencePort, int acknowledgeNum, int dataOff, int rev, bool[] Flags, int Window, int CheckSum, int Urgent, int options, int Padding, int TcpData)
         {
-            BitArray sourcePort = new BitArray(16);
-            BitArray destinationPort = new BitArray(16);
-            BitArray sequencePort = new BitArray(32);
-            BitArray acknowlegementNumber = new BitArray(32);
-            BitArray dataOffset = new BitArray(4);  //needs to be 4 bits
-            BitArray reversed = new BitArray(3); //needs to be 6 bits
-            BitArray controlBits = new BitArray(6); //needs to be 6 bits
-            BitArray CWR = new BitArray(1)
-            ushort window;
-            ushort checkSum;
-            ushort urgentPointer;
-            ushort tcpOptions;
-            ushort padding;
-            ushort tcpData;
+            BitArray SourcePort = new BitArray(BitConverter.GetBytes(sourcePort));
+            BitArray DestinationPort = new BitArray(BitConverter.GetBytes(destinationPort));
+            BitArray SequencePort = new BitArray(BitConverter.GetBytes(sequencePort));
+            BitArray acknowlegementNumber = new BitArray(BitConverter.GetBytes(acknowledgeNum));
+            BitArray dataOffset = new BitArray(BitConverter.GetBytes(dataOff));  //needs to be 4 bits
+            BitArray reversed = new BitArray(BitConverter.GetBytes(rev)); //needs to be 6 bits 
+            BitArray flags = new BitArray(Flags);
+            BitArray window = new BitArray(BitConverter.GetBytes(Window));
+            BitArray checkSum = new BitArray (BitConverter.GetBytes(CheckSum));
+            BitArray urgentPointer = new BitArray(BitConverter.GetBytes(Urgent));
+            BitArray tcpOptions = new BitArray(BitConverter.GetBytes(options));
+            BitArray padding = new BitArray(BitConverter.GetBytes(Padding));
+            BitArray tcpData = new BitArray(BitConverter.GetBytes(TcpData));
 
         }
 
